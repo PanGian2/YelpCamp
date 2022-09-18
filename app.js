12275ae4e5b5944ff398293dd1977361de87563f
Panayiotis Yiannakopoulos <panayiotis.yiannakopoulos@gmail.com>
2022-09-18 20:15:30 +0300

    add index.js

--- app.js
+++ app.js
@@ -80,6 +80,8 @@ const scriptSrcUrls = [
     "https://api.mapbox.com/",
     "https://kit.fontawesome.com/",
     "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js",
     "https://cdn.jsdelivr.net",
 ];
 const styleSrcUrls = [
@@ -89,7 +91,8 @@ const styleSrcUrls = [
     "https://api.tiles.mapbox.com/",
     "https://fonts.googleapis.com/",
     "https://use.fontawesome.com/",
-    "https://cdn.jsdelivr.net"
+    "https://cdn.jsdelivr.net",
+
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,13 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {});
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, {});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -15,47 +21,47 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 150; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
-        const camp = new Campground({
-            author: '62bdde580bae5df110cecd43',
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt id tenetur incidunt quos iure facilis, laudantium doloremque illo. Consequuntur consequatur officiis eos? Animi, id consequuntur. Sit asperiores dolore numquam iste?',
-            price,
-            geometry: {
-                type: "Point",
-                coordinates: [
-                    cities[random1000].longitude,
-                    cities[random1000].latitude,
-                ]
-            },
-            image: [
-                {
-                    url: 'https://res.cloudinary.com/doaxtlc91/image/upload/v1663495183/YelpCamp/IMG_20180813_120538_z0pezd.jpg',
-                    filename: 'YelpCamp/IMG_20180813_120538_z0pezd',
-                },
-                {
-                    url: 'https://res.cloudinary.com/doaxtlc91/image/upload/v1663495139/YelpCamp/IMG_20180315_181907_io9xkk.jpg',
-                    filename: 'YelpCamp/IMG_20180315_181907_io9xkk',
-                },
-                {
-                    url: 'https://res.cloudinary.com/doaxtlc91/image/upload/v1663495146/YelpCamp/IMG_20180316_180601_e7bwla.jpg',
-                    filename: 'YelpCamp/IMG_20180316_180601_e7bwla',
-                },
-                {
-                    url: 'https://res.cloudinary.com/doaxtlc91/image/upload/v1663495267/YelpCamp/IMG_20190414_113055_j7ulrv.jpg',
-                    filename: 'YelpCamp/IMG_20190414_113055_j7ulrv',
-                },
-                {
-                    url: 'https://res.cloudinary.com/doaxtlc91/image/upload/v1663495246/YelpCamp/IMG_20181206_091339_wo3dgz.jpg',
-                    filename: 'YelpCamp/IMG_20181206_091339_wo3dgz',
-                }
-            ]
-        })
-        await camp.save();
-    }
+    // for (let i = 0; i < 150; i++) {
+    //     const random1000 = Math.floor(Math.random() * 1000);
+    //     const price = Math.floor(Math.random() * 20) + 10;
+    //     const camp = new Campground({
+    //         author: '62bdde580bae5df110cecd43',
+    //         location: `${cities[random1000].city}, ${cities[random1000].state}`,
+    //         title: `${sample(descriptors)} ${sample(places)}`,
+    //         description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt id tenetur incidunt quos iure facilis, laudantium doloremque illo. Consequuntur consequatur officiis eos? Animi, id consequuntur. Sit asperiores dolore numquam iste?',
+    //         price,
+    //         geometry: {
+    //             type: "Point",
+    //             coordinates: [
+    //                 cities[random1000].longitude,
+    //                 cities[random1000].latitude,
+    //             ]
+    //         },
+    //         image: [
+    //             {
+    //                 url: 'https://res.cloudinary.com/doaxtlc91/image/upload/v1663495183/YelpCamp/IMG_20180813_120538_z0pezd.jpg',
+    //                 filename: 'YelpCamp/IMG_20180813_120538_z0pezd',
+    //             },
+    //             {
+    //                 url: 'https://res.cloudinary.com/doaxtlc91/image/upload/v1663495139/YelpCamp/IMG_20180315_181907_io9xkk.jpg',
+    //                 filename: 'YelpCamp/IMG_20180315_181907_io9xkk',
+    //             },
+    //             {
+    //                 url: 'https://res.cloudinary.com/doaxtlc91/image/upload/v1663495146/YelpCamp/IMG_20180316_180601_e7bwla.jpg',
+    //                 filename: 'YelpCamp/IMG_20180316_180601_e7bwla',
+    //             },
+    //             {
+    //                 url: 'https://res.cloudinary.com/doaxtlc91/image/upload/v1663495267/YelpCamp/IMG_20190414_113055_j7ulrv.jpg',
+    //                 filename: 'YelpCamp/IMG_20190414_113055_j7ulrv',
+    //             },
+    //             {
+    //                 url: 'https://res.cloudinary.com/doaxtlc91/image/upload/v1663495246/YelpCamp/IMG_20181206_091339_wo3dgz.jpg',
+    //                 filename: 'YelpCamp/IMG_20181206_091339_wo3dgz',
+    //             }
+    //         ]
+    //     })
+    //     await camp.save();
+    //}
 }
 
 seedDB().then(() => {
