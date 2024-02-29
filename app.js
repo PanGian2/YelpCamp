d53c6772bc71d31a3c0a4ab1baf16d7aed4b0abf
Panayiotis Yiannakopoulos <panayiotis.yiannakopoulos@gmail.com>
2024-02-29 21:37:53 +0200

    Initial commit

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 //const dbUrl = process.env.DB_URL;
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = 'mongodb://localhost:27017/yelp-camp'
+// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 
 mongoose.connect(dbUrl, {});
 
